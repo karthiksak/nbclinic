ffaafc4c9d275d03a6bb61b2bae67a04c33ff972
Karthikeyan Sakthivel <kkeyan2411@gmail.com>
2026-07-13 19:52:20 +0530

    fix: restore .eleventy.js as ESM (export default) to fix 404 - Eleventy config was not loading from .cjs

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
   eleventyConfig.addPassthroughCopy("src/images");
